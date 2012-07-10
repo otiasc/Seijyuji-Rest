844625388c0f018654068ce245fc754f8c36c883
Carlos Saito <cosu@hotmail.es>
2012-07-10 02:07:35 +0200

    Sustituir emoticonos por sistema de batalla

---
+++ changeemoticon.js
@@ -0,0 +1,18 @@
+/*--------------------------------------------------------------------
+|                                                                     |
+|   SEIJYUJI GAKUEN PRESENTS                                          |
+|   BATTLE SYSTEM ZERO                                                |
+|                                                                     |
+|   Sustituye los emoticonos por el sistema de batalla                |
+|                                                                     |
+ --------------------------------------------------------------------*/
+$(document).ready(function(){
+	//                                             
+	// 1 Comprobar si pertenece al programa BETA   
+	var user = $('img#i_icon_mini_logout').attr('title');
+	if (user=='Desconectarse [ Admin ]' || user=='Desconectarse [ Ryûji Suguro ]' || user=='Desconectarse [ Okumura Rin ]') {
+		$('#smiley-box iframe').attr('src', '/h7-newbattlesystem');
+	} else {
+		$('#smiley-box iframe').attr('src', '/h4-sistema-de-batalla-entre-usuarios');
+	}
+});
