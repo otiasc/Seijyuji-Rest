66d5b8b6dc8f01b696e710dddf3340d8433fdf69
Carlos Saito <cosu@hotmail.es>
2012-08-15 19:58:07 +0200

    Added Vandrake

--- javascripts/statschecker2.js
+++ javascripts/statschecker2.js
@@ -1,4 +1,4 @@
-/*--------------------------------------------------------------------\
+/*_______________________________________________________________-----\
 |                                                                     |
 |   SEIJYUJI GAKUEN PRESENTS                                          |
 |   SHEET CHECKER                                                     |
@@ -7,7 +7,7 @@
 |   En caso de que se haya hecho, se puede restaurar hasta el punto   |
 |   anterior de guardado                                              |
 |                                                                     |
- --------------------------------------------------------------------*/
+ _______________________________________________________________-----*/
  
  
 var statistics = new Array();
@@ -19,29 +19,29 @@ statistics.push({id:5,  F:'332300010'}); // Shima Renzou
 statistics.push({id:6,  F:'741309600'}); // Night Knightley
 statistics.push({id:9,  F:'114500100'}); // Izumo
 statistics.push({id:10, F:'113600001'}); // Shiemi Moriyama
-//statistics.push({id:--, F:'-------'}); // Shima Kinzou
-//statistics.push({id:--, F:'-------'}); // Shiemi Moriyama
+//statistics.push({id:--, F:'_______'}); // Shima Kinzou
+//statistics.push({id:--, F:'_______'}); // Shiemi Moriyama
 statistics.push({id:15, F:'442336223'}); // Kira Okami
 statistics.push({id:16, F:'533436444'}); // Shura Kirigakure
 statistics.push({id:17, F:'243201000'}); // Natsuky Hikary
-//statistics.push({id:18, F:'-------'}); // Kuroyuki Hiramatsu
+//statistics.push({id:18, F:'_______'}); // Kuroyuki Hiramatsu
 statistics.push({id:19, F:'342201000'}); // Kumiko Yagami
-//statistics.push({id:20, F:'-------'}); // Rain
-//statistics.push({id:21, F:'-------'}); // Perro
-//statistics.push({id:22, F:'-------'}); // Cade666
-//statistics.push({id:23, F:'-------'}); // Takei Zangetsu
-//statistics.push({id:24, F:'-------'}); // Igor Neuhaus
-//statistics.push({id:25, F:'-------'}); // Mephisto
+//statistics.push({id:20, F:'_______'}); // Rain
+//statistics.push({id:21, F:'_______'}); // Perro
+//statistics.push({id:22, F:'_______'}); // Cade666
+//statistics.push({id:23, F:'_______'}); // Takei Zangetsu
+//statistics.push({id:24, F:'_______'}); // Igor Neuhaus
+//statistics.push({id:25, F:'_______'}); // Mephisto
 statistics.push({id:26, F:'434224411'}); // kida kuran
 statistics.push({id:27, F:'244495000'}); // Mr. Fox
-//statistics.push({id:28, F:'-------'}); // Limonxic
-//statistics.push({id:29, F:'-------'}); // Dragon Ball…
+//statistics.push({id:28, F:'_______'}); // Limonxic
+//statistics.push({id:29, F:'_______'}); // Dragon Ball…
 statistics.push({id:30, F:'244100100'}); // Mia Asakura
-//statistics.push({id:31, F:'-------'}); // Sayo
-//statistics.push({id:32, F:'-------'}); // Hikary Ryo
-//statistics.push({id:33, F:'-------'}); // Sayo
-//statistics.push({id:33, F:'-------'}); // Yami D. Noctis
-//statistics.push({id:34, F:'-------'}); // Gosuto Kage
+//statistics.push({id:31, F:'_______'}); // Sayo
+//statistics.push({id:32, F:'_______'}); // Hikary Ryo
+//statistics.push({id:33, F:'_______'}); // Sayo
+//statistics.push({id:33, F:'_______'}); // Yami D. Noctis
+//statistics.push({id:34, F:'_______'}); // Gosuto Kage
 statistics.push({id:35, F:'223400100'}); // Meredith Ariant
-//statistics.push({id:36, F:'-------'}); // Vandrake
+statistics.push({id:36, F:'343356000'}); // Vandrake
 statistics.push({id:37, F:'442244000'}); // Charlotte Blue
