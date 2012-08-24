64f7748043a9be22cb8199b0c3bde47b9ca98b07
Carlos Saito <k_saito_91@hotmail.com>
2012-08-24 03:30:30 +0200

    Core created (v.0)

---
+++ javascripts/ShopSystem/core.js
@@ -0,0 +1,18 @@
+/*
+
+	SHOP-SYSTEM CORE
+	-Herramientas para la confirmación de compra y uso
+	
+*/
+
+// Función buyObject
+function buyObject() {
+	
+}
+
+// Función useObject
+function useObject() {
+	
+}
+
+// Conversor de <span>
