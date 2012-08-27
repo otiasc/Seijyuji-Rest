5bce036805a109339f8275d98fe017f9660c174b
Carlos Saito <k_saito_91@hotmail.com>
2012-08-27 22:38:16 +0200

    a

--- itemSystem.itemsLists.js
+++ itemSystem.itemsLists.js
@@ -1,4 +1,4 @@
-// Lista de objetos
+// Lista de objetos8
 var ALL_ITEMS = new ItemList();
 ALL_ITEMS.addItem(new Item(0, 'Noob Item', 'Item que no sirve para nada', ''));
 ALL_ITEMS.addItem(new Item(1, 'Cartuchos de balas', 'Cada cartucho incluye 8 balas', '', {} ));
